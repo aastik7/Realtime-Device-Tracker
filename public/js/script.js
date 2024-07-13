505e78d725c6351db26ae4cbd4bdc2be20d2e454
Aastik <aastikdude7@gmail.com>
2024-07-13 23:36:03 +0530

    adding markers

--- public/js/script.js
+++ public/js/script.js
@@ -8,11 +8,11 @@ if (navigator.geolocation) {
     },
     (error) => {
       console.error(error);
-      {
-        enableHighAccuracy: true;
-        timeout: 5000;
-        maximumAge: 0;
-      }
+    },
+    {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
     }
   );
 }
@@ -24,3 +24,13 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 const markers = {};
+
+socket.on("receive-location", (data) => {
+  const { id, latitude, longitude } = data;
+  map.setView([latitude, longitude], 16);
+  if (markers[id]) {
+    markers[id].setLatLng([latitude, longitude]);
+  } else {
+    markers[id] = L.marker([latitude, longitude]).addTo(map);
+  }
+});
